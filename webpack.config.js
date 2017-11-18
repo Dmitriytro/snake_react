f1a8cd867f5b257b96c8a1b8c7d08d235a2a3b6f
Dmitriytro <Dmitriytro@gmail.com>
2017-11-18 00:11:59 -0800

    webpack 3 base

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+var path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const extractSass = new ExtractTextPlugin({
+    filename: "[name].[contenthash].css",
+    disable: process.env.NODE_ENV === "development"
+});
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+            test: /\.sass$/,
+            use: extractSass.extract({
+                use: [{
+                    loader: "css-loader"
+                }, {
+                    loader: "sass-loader"
+                }],
+                // use style-loader in development
+                fallback: "style-loader"
+            })
+        }]
+    },
+    plugins: [
+        new ExtractTextPlugin('style.css')
+    ],
+    devServer: {
+        contentBase: path.resolve(__dirname, "dist")
+    }
+};
